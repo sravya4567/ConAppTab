a7c728946f6c18c84d5f52f9dc533cfde1e9ba28
Pranay Gonipalli <pranaygonipalli1303@gmail.com>
2024-02-15 15:44:04 +0530

    Final commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Table of 5");
+int num = 5;
+for (int i = 0; i < num; i++) 
+{
+    Console.WriteLine("{0}*{1}={2}", num,i,(num * i));
+}
